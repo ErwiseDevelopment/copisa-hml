e0a4d4536c0ddf01ca878b5aa0d6cf542ebfc6fc
ErwiseDevelopment <erwise.dev@gmail.com>
2022-12-04 22:40:09 -0300

    ajustes

--- single-post.php
+++ single-post.php
@@ -231,7 +231,7 @@ get_header(); ?>
 											$args = array(
 												'posts_per_page' => 3,
 												'post_type'      => 'post',
-												'category_name'  => 'noticias' || 'blog',
+												'category_name'  => 'noticias',
 												//'order'          => 'DESC',
 												);
 
